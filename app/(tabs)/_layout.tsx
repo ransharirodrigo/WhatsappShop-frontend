a8fe5db3e5d0bc66b219af95cb3c01add37d9634
randikaa <randikapereraaa@gmail.com>
2025-12-14 19:53:53 +0530

    login and signup done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -216,6 +216,7 @@ export default function TabLayout() {
       <Tabs.Screen name="SellerHelpDesk" options={{ href: null }} />
       <Tabs.Screen name="SellerPublishDraft/index" options={{ href: null }} />
       <Tabs.Screen name="SellerPublishDraft/[id]" options={{ href: null }} />
+      <Tabs.Screen name="view-all-products" options={{ href: null }} />
 
       {/* HIDDEN SCREENS OF BUYER */}
       <Tabs.Screen name="HelpDesk" options={{ href: null }} />
